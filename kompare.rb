dcfba28ee678e9fed995803cb64a7444e873b659
Guillaume Baty <guillaume.baty@gmx.fr>
2013-12-31 18:44:49 +0100

    Add formula for kompare

---
+++ kompare.rb
@@ -0,0 +1,11 @@
+require File.join(File.dirname(__FILE__), 'base_kde_formula')
+
+class Kate < BaseKdeFormula
+  homepage 'http://www.caffeinated.me.uk/kompare/'
+  url 'http://download.kde.org/stable/4.11.4/src/kompare-4.11.4.tar.xz'
+  sha1 'b8e4dc115eaf7001b94fd2729804e9015d5c7eeb'
+
+  depends_on 'kdelibs'
+  depends_on 'kde-runtime'
+  kde_build_deps
+end
